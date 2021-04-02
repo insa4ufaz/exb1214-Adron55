ec062720177b61ee30667c3151b1096ba01aa768
Anar Mammadli <>
2021-04-02 13:03:20 +0000

    palindrome function

--- Palindrome.java
+++ Palindrome.java
@@ -2,6 +2,13 @@ public class Palindrome{
     public static boolean is_palindrome(String s)
     {
         // add your code here
-        return true;
+      String rvr = ""; // Objects of String class  
+      int length = s.length();   
+      for ( int i = length - 1; i >= 0; i-- )  
+         rvr = rvr + s.charAt(i);  
+      if (s.equals(rvr))  
+         return true;  
+      else  
+         return false;
     }
 }
